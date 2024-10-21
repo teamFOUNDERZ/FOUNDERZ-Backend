bd5c4d7b6157c3696b3e95016e276e1c9c9393d1
101 <tmddnjs001133@dsm.hs.kr>
2024-10-21 18:55:30 +0900

    add: FounderzException

---
+++ founderz-common/src/main/java/com/founderz/exception/FounderzException.java
@@ -0,0 +1,33 @@
+package com.founderz.exception;
+
+class FounderzException extends RuntimeException {
+    private final int statusCode;
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    protected FounderzException(final int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    protected FounderzException(final String message, final int statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    protected FounderzException(final Throwable throwable, final int statusCode) {
+        super(throwable);
+        this.statusCode = statusCode;
+    }
+
+    protected FounderzException(final String message, final Throwable throwable, final int statusCode) {
+        super(message, throwable);
+        this.statusCode = statusCode;
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
+}
