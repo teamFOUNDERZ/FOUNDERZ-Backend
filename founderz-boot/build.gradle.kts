399407fab248aa28ba24cc82f70c29dcd1d50d91
101 <tmddnjs001133@dsm.hs.kr>
2024-10-21 12:10:16 +0900

    structure: setting multi module

--- founderz-boot/build.gradle.kts
+++ founderz-boot/build.gradle.kts
@@ -1,2 +1,7 @@
 dependencies {
+    implementation(project(":founderz-application"))
+    implementation(project(":founderz-domain"))
+    implementation(project(":founderz-security"))
+    implementation(project(":founderz-application"))
+    implementation(project(":founderz-presentation"))
 }
---
+++ founderz-domain/build.gradle.kts
@@ -0,0 +1,14 @@
+subprojects {
+    val useMySQL: Boolean by extra(false)
+
+    afterEvaluate {
+        dependencies {
+            implementation(project(":board-common"))
+
+            if (useMySQL) {
+                implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+                runtimeOnly("com.mysql:mysql-connector-j")
+            }
+        }
+    }
+}
