7de141d55b76d0ad26f76c13c3c755df020ac235
101 <tmddnjs001133@dsm.hs.kr>
2024-10-21 22:12:40 +0900

    update: assertArgumentNotEmpty 추가

--- founderz-common/src/main/java/com/founderz/assertion/AssertionUtils.java
+++ founderz-common/src/main/java/com/founderz/assertion/AssertionUtils.java
@@ -5,6 +5,12 @@ import com.founderz.exception.BadRequestException;
 import java.util.regex.Pattern;
 
 public class AssertionUtils {
+    public static void assertArgumentNotEmpty(final String aString, final String aMessage) {
+        if (aString == null || aString.trim().isEmpty()) {
+            throw new BadRequestException(aMessage);
+        }
+    }
+
     public static void assertRegularExpression(final String aString, final String regex, final String aMessage) {
         if (!Pattern.matches(regex, aString)) {
             throw new BadRequestException(aMessage);
