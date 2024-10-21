60737bf9fa1ae2759ece80a136e85057a13e73f3
101 <tmddnjs001133@dsm.hs.kr>
2024-10-21 18:56:41 +0900

    update: ErrorResponse 필드 변경

--- founderz-common/src/main/java/com/founderz/response/ErrorResponse.java
+++ founderz-common/src/main/java/com/founderz/response/ErrorResponse.java
@@ -3,16 +3,15 @@ package com.founderz.response;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-public record ErrorResponse<T>(
+public record ErrorResponse(
         int statusCode,
         String message,
-        T detail,
         long timestamp,
         String path
 ) {
     private static final long TIMESTAMP = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
 
-    public static <T> ErrorResponse<T> create(int status, String message, String path) {
-        return new ErrorResponse<>(status, message, null, TIMESTAMP, path);
+    public static ErrorResponse create(int status, String message, String path) {
+        return new ErrorResponse(status, message, TIMESTAMP, path);
     }
 }
