98ac1dee4cabd36eb7d76534ce3f5fe786c0931f
101 <tmddnjs001133@dsm.hs.kr>
2024-10-22 12:28:35 +0900

    refactor: RegexUtils

--- founderz-common/src/main/java/com/founderz/common/assertion/RegexUtils.java
+++ founderz-common/src/main/java/com/founderz/common/assertion/RegexUtils.java
@@ -1,33 +1,101 @@
 package com.founderz.common.assertion;
 
+/**
+ * 자주 사용되는 정규표현식을 위한 유틸리티 열거형 클래스입니다. <br/>
+ * 문자열 검증에 사용되는 정규표현식을 제공합니다.
+ */
 public enum RegexUtils {
-    // 010-1234-1234
-    TEL_REGEX("^01(?:0|1|[6-9])-(\\d{3,4})-(\\d{4})$"),
-    // 8~20자의 영문자, 숫자, 특수문자를 포함
-    PASSWORD_REGEX("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[a-zA-Z\\d!@#$%^&*]"),
+    /**
+     * 전화번호 형식 검증을 위한 정규표현식
+     * <p>형식: 010-0000-0000</p>
+     * <ul>
+     *   <li>시작은 반드시 010, 011로 시작</li>
+     *   <li>중간 자리는 4자리의 숫자</li>
+     *   <li>마지막 자리는 4자리의 숫자</li>
+     * </ul>
+     */
+    TEL("^01(?:0|1)-(\\d{4})-(\\d{4})$"),
 
+    /**
+     * 비밀번호 유효성 검증을 위한 정규표현식
+     * <p>요구사항:</p>
+     * <ul>
+     *   <li>8~20자 길이</li>
+     *   <li>영문자 1개 이상 포함 필수</li>
+     *   <li>숫자 1개 이상 포함 필수</li>
+     *   <li>특수문자 1개 이상 포함 필수 (!@#$%^&*)</li>
+     * </ul>
+     */
+    PASSWORD("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[a-zA-Z\\d!@#$%^&*]"),
+
+    /**
+     * 한글만 허용하는 정규표현식
+     * <p>유니코드 범위: 가-힣</p>
+     */
     ALLOWED_ONLY_KOR("^[가-힣]"),
+
+    /**
+     * 영문자만 허용하는 정규표현식
+     * <p>허용 범위: 대소문자 알파벳 (a-z, A-Z)</p>
+     */
     ALLOWED_ONLY_ENG("^[a-zA-Z]"),
+
+    /**
+     * 한글과 영문자만 허용하는 정규표현식
+     * <p>허용 범위:</p>
+     * <ul>
+     *   <li>한글 (가-힣)</li>
+     *   <li>영문 대소문자 (a-z, A-Z)</li>
+     * </ul>
+     */
     ALLOWED_ONLY_KOR_N_ENG("^[a-zA-Z가-힣]"),
+
+    /**
+     * 영문자와 숫자만 허용하는 정규표현식
+     * <p>허용 범위:</p>
+     * <ul>
+     *   <li>영문 대소문자 (a-z, A-Z)</li>
+     *   <li>숫자 (0-9)</li>
+     * </ul>
+     */
     ALLOWED_ONLY_ENG_N_NUM("^[a-zA-Z0-9]");
 
     private final String regex;
 
+    /**
+     * 정규표현식 패턴을 반환합니다.
+     * <p>
+     * 전화번호(TEL_REGEX)를 제외한 모든 패턴에는 다음과 같은 규칙이 적용됩니다:
+     * <ul>
+     *   <li>하나 이상의 문자 매칭을 위한 + 기호(수량자) 추가</li>
+     *   <li>문자열 끝을 나타내는 $ 기호 추가</li>
+     * </ul>
+     * </p>
+     *
+     * @return 완성된 정규표현식 패턴
+     */
     public String getRegex() {
-        if (!this.equals(TEL_REGEX)) {
-            throw new IllegalStateException("올바르지 않은 객체에서 `getRegex()`를 호출했습니다.");
-        }
-        return regex;
+        return this.equals(TEL)
+                ? regex
+                : regex + "+$";
     }
 
+    /**
+     * 지정된 길이 제한이 포함된 정규표현식을 반환합니다. <br/>
+     * 전화번호 형식을 제외한 모든 정규표현식에 대해 길이 제한을 적용할 수 있습니다.
+     *
+     * @param minLength 최소 길이 (포함)
+     * @param maxLength 최대 길이 (포함)
+     * @return 길이 제한이 적용된 정규표현식 패턴
+     */
     public String getRegex(final int minLength, final int maxLength) {
-        if (this.equals(TEL_REGEX)) {
+        if (this.equals(TEL)) {
             return regex;
         }
         return regex + "{" + minLength + "," + maxLength + "}" + "$";
     }
 
-    RegexUtils(final String regex) {
+    private RegexUtils(final String regex) {
         this.regex = regex;
     }
 }
--- founderz-presentation/auth-presentation/src/main/java/com/founderz/auth/presentation/RegisterForm.java
+++ founderz-presentation/auth-presentation/src/main/java/com/founderz/auth/presentation/RegisterForm.java
@@ -5,8 +5,8 @@ import static com.founderz.common.assertion.AssertionUtils.assertArgumentNotEmpt
 
 import static com.founderz.common.assertion.RegexUtils.ALLOWED_ONLY_ENG_N_NUM;
 import static com.founderz.common.assertion.RegexUtils.ALLOWED_ONLY_KOR;
-import static com.founderz.common.assertion.RegexUtils.TEL_REGEX;
-import static com.founderz.common.assertion.RegexUtils.PASSWORD_REGEX;
+import static com.founderz.common.assertion.RegexUtils.TEL;
+import static com.founderz.common.assertion.RegexUtils.PASSWORD;
 
 record RegisterForm(
         String accountId,
@@ -19,8 +19,8 @@ record RegisterForm(
         assertRegularExpression(accountId, ALLOWED_ONLY_ENG_N_NUM.getRegex(4, 20), "계정 아이디는 4~20자의 영문자와 숫자로만 구성되어야 합니다.");
         assertRegularExpression(name, ALLOWED_ONLY_KOR.getRegex(2, 6), "이름은 2~6자의 한글로 입력되어야 합니다.");
         assertRegularExpression(type, "^(PERSONAL|COMPANY)$", "계정의 유형은 'PERSONAL' 또는 'COMPANY'이어야 합니다.");
-        assertRegularExpression(tel, TEL_REGEX.getRegex(), "전화번호 형식이 유효하지 않습니다. (예: 010-1234-5678)");
-        assertRegularExpression(password, PASSWORD_REGEX.getRegex(8, 20), "비밀번호는 8~20자의 영문자, 숫자, 특수문자를 포함해야 합니다.");
+        assertRegularExpression(tel, TEL.getRegex(), "전화번호 형식이 유효하지 않습니다. (예: 010-1234-5678)");
+        assertRegularExpression(password, PASSWORD.getRegex(8, 20), "비밀번호는 8~20자의 영문자, 숫자, 특수문자를 포함해야 합니다.");
 
         assertArgumentNotEmpty(accountId, "계정의 아이디가 입력되지 않았습니다.");
         assertArgumentNotEmpty(name, "이름이 입력되지 않았습니다.");
