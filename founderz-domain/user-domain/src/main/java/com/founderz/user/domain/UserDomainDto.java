05bbabe461855c7b172f11392278ec821e1575ab
101 <tmddnjs001133@dsm.hs.kr>
2024-10-24 11:03:51 +0900

    update: add assertion

--- founderz-domain/user-domain/src/main/java/com/founderz/user/domain/UserDomainDto.java
+++ founderz-domain/user-domain/src/main/java/com/founderz/user/domain/UserDomainDto.java
@@ -11,10 +11,10 @@ public record UserDomainDto(
         String password
 ) {
     public UserDomainDto {
-        assertArgumentNotEmpty(accountId, "");
-        assertArgumentNotEmpty(name, "");
-        assertArgumentNotEmpty(type, "");
-        assertArgumentNotEmpty(tel, "");
-        assertArgumentNotEmpty(password, "");
+        assertArgumentNotEmpty(accountId, "계정의 아이디가 존재하지 않습니다.");
+        assertArgumentNotEmpty(name, "계정의 이름이 존재하지 않습니다.");
+        assertArgumentNotEmpty(type, "계정의 유형이 존재하지 않습니다.");
+        assertArgumentNotEmpty(tel, "계정의 전화번호가 존재하지 않습니다.");
+        assertArgumentNotEmpty(password, "계정의 비밀번호가 존재하지 않습니다.");
     }
 }
