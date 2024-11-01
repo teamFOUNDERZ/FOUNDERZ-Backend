c9472cb7dfaf714c113b094c3b7cda5cdcbcf3a3
101 <tmddnjs001133@dsm.hs.kr>
2024-11-02 00:18:59 +0900

    add: converter

---
+++ founderz-presentation/src/main/java/com/founderz/presentation/PresentationConfiguration.java
@@ -0,0 +1,13 @@
+package com.founderz.presentation;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+class PresentationConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverterFactory(new StringToValueObjectConverterFactory());
+    }
+}
