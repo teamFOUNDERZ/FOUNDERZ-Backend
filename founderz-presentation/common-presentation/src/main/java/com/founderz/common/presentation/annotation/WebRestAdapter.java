2f26ec76e6671bb1ab8b38ef3001c0b4d65b1aca
101 <tmddnjs001133@dsm.hs.kr>
2024-11-02 22:38:52 +0900

    add: WebRestAdapter custom annotation

---
+++ founderz-presentation/common-presentation/src/main/java/com/founderz/common/presentation/annotation/WebRestAdapter.java
@@ -0,0 +1,21 @@
+package com.founderz.common.presentation.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@RestController
+@RequestMapping
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface WebRestAdapter {
+    @AliasFor(annotation = RequestMapping.class, attribute = "path")
+    String value() default "";
+}
