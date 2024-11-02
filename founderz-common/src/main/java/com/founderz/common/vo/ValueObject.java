119ee39a88444b98e6c03f5d5a8132f06028f282
101 <tmddnjs001133@dsm.hs.kr>
2024-11-02 15:17:42 +0900

    update: String, Long 분리 후 sealed, permits 키워드 사용

--- founderz-common/src/main/java/com/founderz/common/vo/ValueObject.java
+++ founderz-common/src/main/java/com/founderz/common/vo/ValueObject.java
@@ -1,14 +1,21 @@
 package com.founderz.common.vo;
 
-public sealed interface ValueObject permits
-        AccountId,
-        AccountIdentifier,
-        AccountName,
-        AccountType,
-        PasetoToken,
-        Password,
-        PhoneNumber,
-        UserId,
-        SecuredPassword,
-        SecuredUserId
-{}
+public sealed interface ValueObject permits ValueObject.LongValueObject, ValueObject.StringValueObject
+{
+    sealed interface StringValueObject extends ValueObject permits
+            AccountId,
+            AccountIdentifier,
+            AccountName,
+            AccountType,
+            PasetoToken,
+            Password,
+            PhoneNumber,
+            SecuredPassword,
+            SecuredUserId {
+    }
+
+    sealed interface LongValueObject extends ValueObject permits
+            UserId {
+    }
+}
+
