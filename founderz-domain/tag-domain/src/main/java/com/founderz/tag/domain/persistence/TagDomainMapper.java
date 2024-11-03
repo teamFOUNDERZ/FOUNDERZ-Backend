102411a1bd50e9c26cc1690dbfe4ead36dea88ef
101 <tmddnjs001133@dsm.hs.kr>
2024-11-03 23:28:21 +0900

    add: TagDomainMapper

---
+++ founderz-domain/tag-domain/src/main/java/com/founderz/tag/domain/persistence/TagDomainMapper.java
@@ -0,0 +1,24 @@
+package com.founderz.tag.domain.persistence;
+
+import com.founderz.tag.domain.dto.TagDomainDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.Optional;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+import static org.mapstruct.ReportingPolicy.ERROR;
+
+@Mapper(componentModel = SPRING, unmappedTargetPolicy = ERROR)
+interface TagDomainMapper {
+    @Mapping(target = "id", expression = "java(TagEntityId.create(dto.id(),dto.name()))")
+    TagEntity toEntity(TagDomainDto dto);
+
+    @Mapping(target = "id", expression = "java(TagId.create(entity.id().id()))")
+    @Mapping(target = "name", expression = "java(TagName.create(entity.id().name()))")
+    TagDomainDto toDto(TagEntity entity);
+
+    default Optional<TagDomainDto> toOptionalDto(Optional<TagEntity> entity) {
+        return entity.map(this::toDto);
+    }
+}
