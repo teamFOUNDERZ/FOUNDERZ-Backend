1deeeee6f76379295672e084d2cda7bedb8bb01a
dkflfkd53 <b57062562@gmail.com>
2024-11-05 19:25:45 +0900

    add: business domain persistence 계층 추가

--- founderz-domain/business-domain/src/main/java/com/founderz/business/domain/persistence/BusinessDomainMapper.java
+++ founderz-domain/business-domain/src/main/java/com/founderz/business/domain/persistence/BusinessDomainMapper.java
@@ -1,4 +1,33 @@
 package com.founderz.business.domain.persistence;
 
-public interface BusinessDomainMapper {
+import com.founderz.business.domain.dto.BusinessDomainDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.Optional;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+import static org.mapstruct.ReportingPolicy.ERROR;
+
+@Mapper(componentModel = SPRING, unmappedTargetPolicy = ERROR)
+interface BusinessDomainMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "accountId", expression = "java(dto.accountId().accountId())")
+    @Mapping(target = "name", expression = "java(dto.name().name())")
+    @Mapping(target = "type", expression = "java(UserEntity.Type.valueOf(dto.type().type()))")
+    @Mapping(target = "phoneNumber", expression = "java(dto.phoneNumber().phoneNumber())")
+    @Mapping(target = "password", expression = "java(dto.securedPassword().password())")
+    BusinessEntity toEntity(BusinessDomainDto dto);
+
+    @Mapping(target = "id", expression = "java(UserId.create(entity.getId()))")
+    @Mapping(target = "accountId", expression = "java(AccountId.create(entity.getAccountId()))")
+    @Mapping(target = "name", expression = "java(AccountName.create(entity.getName()))")
+    @Mapping(target = "type", expression = "java(AccountType.create(entity.getType().name()))")
+    @Mapping(target = "phoneNumber", expression = "java(PhoneNumber.create(entity.getPhoneNumber()))")
+    @Mapping(target = "securedPassword", expression = "java(SecuredPassword.create(entity.getPassword()))")
+    BusinessDomainDto toDto(BusinessEntity entity);
+
+    default Optional<BusinessDomainDto> toOptionalDto(Optional<BusinessEntity> entity) {
+        return entity.map(this::toDto);
+    }
 }
--- founderz-domain/business-domain/src/main/java/com/founderz/business/domain/persistence/BusinessJpaRepository.java
+++ founderz-domain/business-domain/src/main/java/com/founderz/business/domain/persistence/BusinessJpaRepository.java
@@ -1,4 +1,6 @@
 package com.founderz.business.domain.persistence;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface BusinessJpaRepository extends JpaRepository<BusinessEntity, Long> {
 }
