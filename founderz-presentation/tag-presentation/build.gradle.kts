e4ac463ddc7c045ee516b21aafc3c82fafbf475e
101 <tmddnjs001133@dsm.hs.kr>
2024-11-05 17:18:55 +0900

    update: build gradle

--- founderz-presentation/tag-presentation/build.gradle.kts
+++ founderz-presentation/tag-presentation/build.gradle.kts
@@ -1,4 +1,3 @@
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation(project(":founderz-application:tag-application"))
 }
