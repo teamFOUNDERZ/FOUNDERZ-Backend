c73cbfd427942b697aaf424e8c5cf8653b577d29
dkflfkd53 <b57062562@gmail.com>
2024-11-06 11:44:39 +0900

    refactor: @Setter 어노테이션 제거

--- founderz-domain/business-domain/src/main/java/com/founderz/business/domain/persistence/BusinessEntity.java
+++ founderz-domain/business-domain/src/main/java/com/founderz/business/domain/persistence/BusinessEntity.java
@@ -8,9 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
 @Getter
 @Entity
 @NoArgsConstructor
