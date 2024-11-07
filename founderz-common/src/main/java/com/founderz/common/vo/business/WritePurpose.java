a97e3ca5060200b69976f0b7d28403bf9c1e717a
김승원 <tmddnjs001133@dsm.hs.kr>
2024-11-07 11:41:14 +0900

    refactor: WritePurpose.java
    사용되지 않는 코드를 삭제하였습니다.

--- founderz-common/src/main/java/com/founderz/common/vo/business/WritePurpose.java
+++ founderz-common/src/main/java/com/founderz/common/vo/business/WritePurpose.java
@@ -3,18 +3,17 @@ package com.founderz.common.vo.business;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.founderz.common.vo.ValueObject;
 
-import java.util.regex.Pattern;
-
-import static com.founderz.common.assertion.AssertionUtils.*;
-import static com.founderz.common.assertion.RegexUtils.ALLOWED_ONLY_KOR;
+import static com.founderz.common.assertion.AssertionUtils.assertArgumentNotEmpty;
+import static com.founderz.common.assertion.AssertionUtils.assertArgumentLength;
 
 public record WritePurpose(
         @JsonValue
         String writePurpose
 ) implements ValueObject.StringValueObject {
+        
     public WritePurpose {
         assertArgumentNotEmpty(writePurpose, "작성 목적이 입력되지 않았습니다.");
-        assertArgumentLength(writePurpose, 5, 100, "작성 목적은 5~100자의 한글로 입력되어야 합니다.");
+        assertArgumentLength(writePurpose, 5, 100, "작성 목적은 5~100자로 입력되어야 합니다.");
     }
 
     public static WritePurpose create(final String writePurpose) {
