1baf1d9efe71efe431105cf744cb3df76d1bee44
101 <tmddnjs001133@dsm.hs.kr>
2024-11-07 14:48:22 +0900

    fix: setter 어노테이션 추가

--- founderz-domain/business-domain/src/main/java/com/founderz/business/domain/persistence/BusinessEntity.java
+++ founderz-domain/business-domain/src/main/java/com/founderz/business/domain/persistence/BusinessEntity.java
@@ -8,7 +8,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Setter
 @Getter
 @Entity
 @NoArgsConstructor
