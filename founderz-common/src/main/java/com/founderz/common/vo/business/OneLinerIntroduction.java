622f5f96f0e70cd4cb4bcf17a123b764f8e527dd
김승원 <tmddnjs001133@dsm.hs.kr>
2024-11-07 11:39:43 +0900

    refactor: OneLinerIntroduction.java
    필드명을 올바르게 수정하였습니다.

--- founderz-common/src/main/java/com/founderz/common/vo/business/OneLinerIntroduction.java
+++ founderz-common/src/main/java/com/founderz/common/vo/business/OneLinerIntroduction.java
@@ -3,22 +3,20 @@ package com.founderz.common.vo.business;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.founderz.common.vo.ValueObject;
 
-import java.util.regex.Pattern;
-
 import static com.founderz.common.assertion.AssertionUtils.assertArgumentNotEmpty;
 import static com.founderz.common.assertion.AssertionUtils.assertArgumentLength;
 
 public record OneLinerIntroduction(
         @JsonValue
-        String introduction
+        String oneLinerIntroduction
 ) implements ValueObject.StringValueObject {
         
     public OneLinerIntroduction {
-        assertArgumentNotEmpty(introduction, "소개가 입력되지 않았습니다.");
-        assertArgumentLength(introduction, 3, 100, "소개는 3~100자로 입력되어야 합니다.");
+        assertArgumentNotEmpty(oneLinerIntroduction, "소개가 입력되지 않았습니다.");
+        assertArgumentLength(oneLinerIntroduction, 3, 100, "소개는 3~100자로 입력되어야 합니다.");
     }
 
-    public static OneLinerIntroduction create(final String introduction) {
-        return new OneLinerIntroduction(introduction);
+    public static OneLinerIntroduction create(final String oneLinerIntroduction) {
+        return new OneLinerIntroduction(oneLinerIntroduction);
     }
 }
