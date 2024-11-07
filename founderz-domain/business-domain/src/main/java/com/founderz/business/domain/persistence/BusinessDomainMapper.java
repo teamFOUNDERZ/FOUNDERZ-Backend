44c1576726c62fbff3989d3d8d58f271f951dad0
101 <tmddnjs001133@dsm.hs.kr>
2024-11-07 15:07:44 +0900

    fix: add get prefix

--- founderz-domain/business-domain/src/main/java/com/founderz/business/domain/persistence/BusinessDomainMapper.java
+++ founderz-domain/business-domain/src/main/java/com/founderz/business/domain/persistence/BusinessDomainMapper.java
@@ -21,14 +21,14 @@ interface BusinessDomainMapper {
     @Mapping(target = "writerAccountId", expression = "java(dto.writerAccountId().accountId())")
     BusinessEntity toEntity(BusinessDomainDto dto);
 
-    @Mapping(target = "id", expression = "java(BusinessId.create(entity.id()))")
-    @Mapping(target = "businessName", expression = "java(BusinessName.create(entity.businessName()))")
-    @Mapping(target = "oneLineIntroduction", expression = "java(OneLineIntroduction.create(entity.oneLineIntroduction()))")
-    @Mapping(target = "businessIntroduction", expression = "java(BusinessIntroduction.create(entity.businessIntroduction()))")
-    @Mapping(target = "vision", expression = "java(Vision.create(entity.vision()))")
-    @Mapping(target = "writePurpose", expression = "java(WritePurpose.create(entity.writePurpose()))")
-    @Mapping(target = "investmentAmount", expression = "java(InvestmentAmount.create(entity.investmentAmount()))")
-    @Mapping(target = "writerAccountId", expression = "java(WriterAccountId.create(entity.writerAccountId()))")
+    @Mapping(target = "id", expression = "java(BusinessId.create(entity.getId()))")
+    @Mapping(target = "businessName", expression = "java(BusinessName.create(entity.getBusinessName()))")
+    @Mapping(target = "oneLineIntroduction", expression = "java(OneLineIntroduction.create(entity.getOneLineIntroduction()))")
+    @Mapping(target = "businessIntroduction", expression = "java(BusinessIntroduction.create(entity.getBusinessIntroduction()))")
+    @Mapping(target = "vision", expression = "java(Vision.create(entity.getVision()))")
+    @Mapping(target = "writePurpose", expression = "java(WritePurpose.create(entity.getWritePurpose()))")
+    @Mapping(target = "investmentAmount", expression = "java(InvestmentAmount.create(entity.getInvestmentAmount()))")
+    @Mapping(target = "writerAccountId", expression = "java(WriterAccountId.create(entity.getWriterAccountId()))")
     BusinessDomainDto toDto(BusinessEntity entity);
 
     default Optional<BusinessDomainDto> toOptionalDto(Optional<BusinessEntity> entity) {
