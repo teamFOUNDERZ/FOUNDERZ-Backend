e9a2252e4136e6a5718ab41a5af8b1fa7a189b8a
김승원 <tmddnjs001133@dsm.hs.kr>
2024-11-07 11:27:54 +0900

    update: OneLinerIntroduction.java
    assertArgumentLength 사용, 기존 한글만 허용했던 정규표현식 제거.

--- founderz-common/src/main/java/com/founderz/common/vo/business/OneLinerIntroduction.java
+++ founderz-common/src/main/java/com/founderz/common/vo/business/OneLinerIntroduction.java
@@ -6,18 +6,16 @@ import com.founderz.common.vo.ValueObject;
 import java.util.regex.Pattern;
 
 import static com.founderz.common.assertion.AssertionUtils.assertArgumentNotEmpty;
-import static com.founderz.common.assertion.AssertionUtils.assertRegularExpression;
-import static com.founderz.common.assertion.RegexUtils.ALLOWED_ONLY_KOR;
+import static com.founderz.common.assertion.AssertionUtils.assertArgumentLength;
 
 public record OneLinerIntroduction(
         @JsonValue
         String introduction
 ) implements ValueObject.StringValueObject {
-    private static final Pattern ONE_LINER_INTRODUCTION_REGEX = Pattern.compile(ALLOWED_ONLY_KOR.getRegex(3, 100));
-
+        
     public OneLinerIntroduction {
         assertArgumentNotEmpty(introduction, "소개가 입력되지 않았습니다.");
-        assertRegularExpression(introduction, ONE_LINER_INTRODUCTION_REGEX, "소개는 3~100자의 한글로 입력되어야 합니다.");
+        assertArgumentLength(introduction, 3, 100, "소개는 3~100자로 입력되어야 합니다.");
     }
 
     public static OneLinerIntroduction create(final String introduction) {
