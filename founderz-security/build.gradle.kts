5443bed649881d1bfc65ed0a1074a31b2253bdfb
101 <tmddnjs001133@dsm.hs.kr>
2024-11-08 19:44:52 +0900

    fix: paseto impl 및 의존성 추가

--- founderz-security/build.gradle.kts
+++ founderz-security/build.gradle.kts
@@ -2,7 +2,10 @@ val pasetoVersion = "0.7.0"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("dev.paseto:jpaseto-its:$pasetoVersion")
+    implementation("dev.paseto:jpaseto-api:$pasetoVersion")
+    implementation("dev.paseto:jpaseto-impl:$pasetoVersion")
+    implementation("dev.paseto:jpaseto-jackson:$pasetoVersion")
+    implementation("dev.paseto:jpaseto-bouncy-castle:$pasetoVersion")
     implementation(project(":founderz-common"))
     implementation(project(":founderz-domain:user-domain"))
     implementation("org.springframework.boot:spring-boot-starter-aop")
