c3e4defdcbb52c708a0d128d1587cfbd7c96e129
101 <tmddnjs001133@dsm.hs.kr>
2024-11-08 22:21:58 +0900

    add: CurrentUser 내부 기능 추가

---
+++ founderz-common/src/main/java/com/founderz/internal/function/security/CurrentUser.java
@@ -0,0 +1,7 @@
+package com.founderz.internal.function.security;
+
+import com.founderz.internal.data.user.CurrentUserInfo;
+
+public interface CurrentUser {
+    CurrentUserInfo get();
+}
--- founderz-security/src/main/java/com/founderz/security/current/CurrentUserImpl.java
+++ founderz-security/src/main/java/com/founderz/security/current/CurrentUserImpl.java
@@ -1,21 +1,31 @@
 package com.founderz.security.current;
 
-import com.founderz.common.security.CurrentUser;
-import com.founderz.common.vo.user.UserId;
+import com.founderz.common.exception.UnauthorizedException;
+import com.founderz.common.vo.user.AccountId;
+import com.founderz.internal.data.user.CurrentUserInfo;
+import com.founderz.internal.function.security.CurrentUser;
 import com.founderz.user.domain.UserDomainReader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 class CurrentUserImpl implements CurrentUser {
     private final UserDomainReader userDomainReader;
+    private final CurrentUserMapper mapper;
 
     @Override
-    public UserId get() {
-        final var userId = SecurityContextHolder.getContext().getAuthentication().getName();
+    @Transactional(readOnly = true)
+    public CurrentUserInfo get() {
+        final var accountId = SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getName();
 
-        return null;
+        final var userDto = userDomainReader.findByAccountId(AccountId.create(accountId))
+                .orElseThrow(() -> new UnauthorizedException("인증에 실패하였습니다."));
+
+        return mapper.toCurrentUserInfo(userDto);
     }
 }
