6a695a493aff7d5b7d01768704b63cdb0d2124d1
101 <tmddnjs001133@dsm.hs.kr>
2024-11-11 08:49:24 +0900

    add: UserInterestDomainMapper

---
+++ founderz-domain/userinterest-domain/src/main/java/com/founderz/userinterest/domain/persistence/UserInterestDomainMapper.java
@@ -0,0 +1,34 @@
+package com.founderz.userinterest.domain.persistence;
+
+import com.founderz.internal.data.userinterest.UserInterestDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+import static org.mapstruct.ReportingPolicy.ERROR;
+
+// UserInterestDto.java
+//        UserInterestId userInterestId,
+//        UserId userId,
+//        TagId tagId,
+//        TagName tagName
+//
+// UserInterestEntity.java
+//        Long id,
+//        Long userId,
+//        Long tagId,
+//        String cacheTagName
+@Mapper(componentModel = SPRING, unmappedTargetPolicy = ERROR)
+interface UserInterestDomainMapper {
+    @Mapping(target = "id", expression = "java(dto.userInterestId().userInterestId())", ignore = true)
+    @Mapping(target = "userId", expression = "java(dto.userId().userId())")
+    @Mapping(target = "tagId", expression = "java(dto.tagId().tagId())")
+    @Mapping(target = "tagName", expression = "java(dto.tagName().tagName())")
+    UserInterestEntity toEntity(UserInterestDto dto);
+
+    @Mapping(target = "userInterestId", expression = "java(UserInterestId.create(entity.getId()))")
+    @Mapping(target = "userId", expression = "java(UserId.create(entity.getUserId()))")
+    @Mapping(target = "tagId", expression = "java(TagId.create(entity.getTagId()))")
+    @Mapping(target = "tagName", expression = "java(TagName.create(entity.getTagName()))")
+    UserInterestDto toDto(UserInterestEntity entity);
+}
