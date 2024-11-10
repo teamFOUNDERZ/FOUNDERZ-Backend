9212bc8acd5bb8381cc89e730be462baf1029e47
101 <tmddnjs001133@dsm.hs.kr>
2024-11-11 08:49:02 +0900

    add: UserInterestEntity

---
+++ founderz-domain/userinterest-domain/src/main/java/com/founderz/userinterest/domain/persistence/UserInterestEntity.java
@@ -0,0 +1,32 @@
+package com.founderz.userinterest.domain.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "founderz_user_interest_v1")
+class UserInterestEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long tagId;
+
+    @Column(nullable = false, length = 10)
+    private String cacheTagName;
+
+}
