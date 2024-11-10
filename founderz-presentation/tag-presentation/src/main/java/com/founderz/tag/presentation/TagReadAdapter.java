80915ea6a6e8a2b851d95298a0447c91eab0e651
101 <tmddnjs001133@dsm.hs.kr>
2024-11-10 21:43:21 +0900

    update: API 개발

--- founderz-presentation/tag-presentation/src/main/java/com/founderz/tag/presentation/TagReadAdapter.java
+++ founderz-presentation/tag-presentation/src/main/java/com/founderz/tag/presentation/TagReadAdapter.java
@@ -1,7 +1,23 @@
 package com.founderz.tag.presentation;
 
 import com.founderz.common.presentation.annotation.WebRestAdapter;
+import com.founderz.tag.application.TagReadService;
+import com.founderz.tag.presentation.document.TagReadDocumentation;
+import com.founderz.tag.presentation.response.TagResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @WebRestAdapter("/api/tags")
-class TagReadAdapter {
+class TagReadAdapter implements TagReadDocumentation {
+    private final TagReadService tagReadService;
+
+    @GetMapping
+    public List<TagResponse> getAll() {
+        return tagReadService.getAll().stream()
+                .map(TagResponse::create)
+                .toList();
+    }
 }
