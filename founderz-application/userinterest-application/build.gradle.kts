56ac0dca37a67db3785dc22ee6fb428c432ad2af
101 <tmddnjs001133@dsm.hs.kr>
2024-11-12 02:19:45 +0900

    build: domain 참조

--- founderz-application/userinterest-application/build.gradle.kts
+++ founderz-application/userinterest-application/build.gradle.kts
@@ -1,19 +1,3 @@
-plugins {
-    id("java")
-}
-
-group = "com.founderz.userinterest.application"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation(project(":founderz-domain:userinterest-domain"))
 }
