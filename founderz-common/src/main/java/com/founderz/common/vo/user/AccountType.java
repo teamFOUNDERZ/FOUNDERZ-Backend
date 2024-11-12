78ff26e1c87cf0da17abef028c0d94b33837de55
101 <tmddnjs001133@dsm.hs.kr>
2024-11-12 19:24:49 +0900

    refactor: user role

--- founderz-common/src/main/java/com/founderz/common/vo/user/AccountType.java
+++ founderz-common/src/main/java/com/founderz/common/vo/user/AccountType.java
@@ -1,22 +1,22 @@
 package com.founderz.common.vo.user;
 
 import com.fasterxml.jackson.annotation.JsonValue;
-import com.founderz.common.exception.BadRequestException;
-import com.founderz.common.security.UserRole;
 import com.founderz.common.vo.ValueObject;
 
+import java.util.regex.Pattern;
+
 import static com.founderz.common.assertion.AssertionUtils.assertArgumentNotEmpty;
+import static com.founderz.common.assertion.AssertionUtils.assertRegularExpression;
 
 public record AccountType(
         @JsonValue
         String type
 ) implements ValueObject.StringValueObject {
+    private static final Pattern USER_ROLE_REGEX = Pattern.compile("^(PERSONAL|COMPANY|ADMIN)");
+
     public AccountType {
         assertArgumentNotEmpty(type, "계정의 유형이 입력되지 않았습니다.");
-
-        if (!UserRole.contains(type)) {
-            throw new BadRequestException("계정의 유형은 'PERSONAL','COMPANY', 'ADMIN' 이어야 합니다.");
-        }
+        assertRegularExpression(type, USER_ROLE_REGEX, "계정의 유형은 'PERSONAL','COMPANY', 'ADMIN' 이어야 합니다.");
     }
 
     public static AccountType create(final String type) {
