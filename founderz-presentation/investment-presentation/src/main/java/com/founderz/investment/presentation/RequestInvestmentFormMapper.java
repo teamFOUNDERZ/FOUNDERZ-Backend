eed73a79d4f4ceac412037cab1f09ad6eb596c8f
dkflfkd53 <b57062562@gmail.com>
2024-11-18 02:16:36 +0900

    add: 투자 요청하기 api form 추가

---
+++ founderz-presentation/investment-presentation/src/main/java/com/founderz/investment/presentation/RequestInvestmentFormMapper.java
@@ -0,0 +1,20 @@
+package com.founderz.investment.presentation;
+
+import com.founderz.internal.data.investment.InvestmentDto;
+import com.founderz.investment.presentation.form.RequestInvestmentForm;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+import static org.mapstruct.ReportingPolicy.ERROR;
+
+@Mapper(componentModel = SPRING, unmappedTargetPolicy = ERROR)
+interface RequestInvestmentFormMapper {
+    @Mapping(target = "investmentId", ignore = true)
+    @Mapping(target = "businessId", source = "businessId")
+    @Mapping(target = "investorName", ignore = true)
+    @Mapping(target = "investmentAmount", source = "investmentAmount")
+    @Mapping(target = "contact", source = "contact")
+    @Mapping(target = "preferContractPeriod", source = "preferContractPeriod")
+    InvestmentDto toDto(RequestInvestmentForm form);
+}
