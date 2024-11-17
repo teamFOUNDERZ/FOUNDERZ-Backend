9261973718953378ddcfb45265e0503daf7e4181
dkflfkd53 <b57062562@gmail.com>
2024-11-18 06:11:56 +0900

    add: 다른 application 로직 주입

--- founderz-application/investment-application/build.gradle.kts
+++ founderz-application/investment-application/build.gradle.kts
@@ -1,3 +1,5 @@
 dependencies {
     implementation(project(":founderz-domain:investment-domain"))
+    implementation(project(":founderz-application:business-application"))
+    implementation(project(":founderz-application:user-application"))
 }
