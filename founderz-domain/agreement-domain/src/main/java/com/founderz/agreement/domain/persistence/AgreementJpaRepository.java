0768e97ba4cd469c09c7cddc1907294bed93a1dc
dkflfkd53 <b57062562@gmail.com>
2024-11-18 00:09:49 +0900

    add: 투자 계약서 관련 domain 계층 추가

---
+++ founderz-domain/agreement-domain/src/main/java/com/founderz/agreement/domain/persistence/AgreementJpaRepository.java
@@ -0,0 +1,6 @@
+package com.founderz.agreement.domain.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgreementJpaRepository extends JpaRepository<AgreementEntity, Long> {
+}
