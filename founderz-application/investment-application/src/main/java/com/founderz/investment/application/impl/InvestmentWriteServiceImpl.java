9824e355783f88ca5775b153e8a118412ba01043
dkflfkd53 <b57062562@gmail.com>
2024-11-18 06:12:18 +0900

    refactor: 투자 요청하기 api 수정

--- founderz-application/investment-application/src/main/java/com/founderz/investment/application/impl/InvestmentWriteServiceImpl.java
+++ founderz-application/investment-application/src/main/java/com/founderz/investment/application/impl/InvestmentWriteServiceImpl.java
@@ -1,9 +1,11 @@
 package com.founderz.investment.application.impl;
 
+import com.founderz.business.application.BusinessReadService;
 import com.founderz.internal.data.investment.InvestmentDto;
 import com.founderz.internal.function.security.CurrentUser;
 import com.founderz.investment.application.InvestmentWriteService;
 import com.founderz.investment.domain.InvestmentDomainWriter;
+import com.founderz.user.application.UserReadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,9 +16,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class InvestmentWriteServiceImpl implements InvestmentWriteService {
     private final InvestmentDomainWriter writer;
     private final CurrentUser currentUser;
+    private final BusinessReadService businessReadService;
+    private final UserReadService userReadService;
 
     @Override
     public void request(final InvestmentDto dto) {
-        writer.save(dto.setInvestorAccountId(currentUser.get().accountId()));
+        final var business = businessReadService.getById(dto.businessId());
+        final var investee = userReadService.getByAccountId(business.writerAccountId());
+
+        writer.save(dto.initInvestmentDto(business.businessName(), currentUser.get().accountId(), currentUser.get().name(), investee.name()));
     }
 }
