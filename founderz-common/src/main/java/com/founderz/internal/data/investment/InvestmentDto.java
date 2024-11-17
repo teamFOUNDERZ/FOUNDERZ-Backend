56b5cb92c0f176d400886b4d591894e581fbba5d
dkflfkd53 <b57062562@gmail.com>
2024-11-18 05:37:07 +0900

    refactor: investmentDto 수정

--- founderz-common/src/main/java/com/founderz/internal/data/investment/InvestmentDto.java
+++ founderz-common/src/main/java/com/founderz/internal/data/investment/InvestmentDto.java
@@ -1,17 +1,22 @@
 package com.founderz.internal.data.investment;
 
 import com.founderz.common.vo.business.BusinessId;
+import com.founderz.common.vo.business.BusinessName;
 import com.founderz.common.vo.business.InvestmentAmount;
 import com.founderz.common.vo.investment.Contact;
 import com.founderz.common.vo.investment.InvestmentId;
 import com.founderz.common.vo.investment.InvestmentStatus;
 import com.founderz.common.vo.investment.PreferContractPeriod;
 import com.founderz.common.vo.user.AccountId;
+import com.founderz.common.vo.user.AccountName;
 
 public record InvestmentDto(
         InvestmentId investmentId,
         BusinessId businessId,
+        BusinessName businessName,
         AccountId investorAccountId,
+        AccountName investorName,
+        AccountName investeeName,
         InvestmentStatus investmentStatus,
         InvestmentAmount investmentAmount,
         Contact contact,
@@ -27,7 +32,10 @@ public record InvestmentDto(
         return new InvestmentDto(
                 investmentId,
                 businessId,
+                businessName,
                 investorAccountId,
+                investorName,
+                investeeName,
                 investmentStatus,
                 investmentAmount,
                 contact,
