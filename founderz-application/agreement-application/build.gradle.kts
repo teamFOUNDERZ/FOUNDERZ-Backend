3b53befc7f12f6a711a16d18d5c633d21b8853a2
dkflfkd53 <b57062562@gmail.com>
2024-11-18 20:24:18 +0900

    add: 자금 투자 계약서 작성하기 api 알림 추가하기

--- founderz-application/agreement-application/build.gradle.kts
+++ founderz-application/agreement-application/build.gradle.kts
@@ -1,4 +1,6 @@
 dependencies {
     implementation(project(":founderz-domain:agreement-domain"))
     implementation(project(":founderz-application:investment-application"))
+    implementation(project(":founderz-application:user-application"))
+    implementation(project(":founderz-application:business-application"))
 }
--- founderz-application/agreement-application/src/main/java/com/founderz/agreement/application/impl/AgreementWriteServiceImpl.java
+++ founderz-application/agreement-application/src/main/java/com/founderz/agreement/application/impl/AgreementWriteServiceImpl.java
@@ -2,11 +2,14 @@ package com.founderz.agreement.application.impl;
 
 import com.founderz.agreement.application.AgreementWriteService;
 import com.founderz.agreement.domain.AgreementDomainWriter;
+import com.founderz.business.application.BusinessReadService;
 import com.founderz.common.vo.notice.NoticeContent;
 import com.founderz.common.vo.notice.NoticeType;
 import com.founderz.internal.data.agreement.AgreementDto;
 import com.founderz.internal.event.NoticeAddEvent;
+import com.founderz.investment.application.InvestmentReadService;
 import com.founderz.investment.application.InvestmentWriteService;
+import com.founderz.user.application.UserReadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -18,21 +21,38 @@ import org.springframework.transaction.annotation.Transactional;
 public class AgreementWriteServiceImpl implements AgreementWriteService {
     private final AgreementDomainWriter writer;
     private final InvestmentWriteService investmentWriteService;
+    private final InvestmentReadService investmentReadService;
+    private final UserReadService userReadService;
     private final ApplicationEventPublisher eventPublisher;
+    private final BusinessReadService businessReadService;
 
     public void write(AgreementDto dto) {
         writer.save(dto);
+        final var investment = investmentReadService.getById(dto.investmentId());
+        final var business = businessReadService.getById(investment.businessId());
+        final var investor = userReadService.getByAccountId(investment.investorAccountId());
+        final var investee = userReadService.getByAccountId(business.writerAccountId());
+
+        if ("WRITING".equals(investment.investmentStatus().status()) ||
+                "REJECTED".equals(investment.investmentStatus().status())) {
+            eventPublisher.publishEvent(NoticeAddEvent.create(
+                    NoticeType.INVESTMENT_AGREEMENT_REVIEW,
+                    investor.userId(),
+                    NoticeContent.create(String.format(
+                            "\"%s\"의 %s님이 자금 투자 계약서 작성을 마쳤어요.\n이상 내용이 없는지 확인해 보세요.",
+                            investment.businessName(),
+                            investment.investeeName()
+                    ))));
+        } else if(investment.investmentStatus().status().equals("COMPLETE")) {
+            eventPublisher.publishEvent(NoticeAddEvent.create(
+                    NoticeType.INVESTMENT_AGREEMENT_MODIFY_REQUEST,
+                    investee.userId(),
+                    NoticeContent.create(String.format(
+                            "%s님이 자금 투자 계약서 수정을 요청했어요.",
+                            investor.name()
+                    ))));
+        }
 
         investmentWriteService.changeStatus(dto.investmentId());
-
-//        eventPublisher.publishEvent(NoticeAddEvent.create(
-//                NoticeType.INVESTMENT_AGREEMENT_REVIEW,
-//                investee.userId(),
-//                NoticeContent.create(String.format(
-//                        "%s님이 \"%s\"에 %l원 투자를 원하고 있어요.",
-//                        investor.name(),
-//                        business.businessName(),
-//                        dto.investmentAmount())
-//                )));
     }
 }
