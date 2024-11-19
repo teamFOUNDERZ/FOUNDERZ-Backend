1aa36a3514aabae0455877b9ed8df8e1c82ed3f5
dkflfkd53 <b57062562@gmail.com>
2024-11-19 13:39:41 +0900

    add: 자금 투자 계약서 조회 api swagger 문서 추가

--- founderz-presentation/agreement-presentation/src/main/java/com/founderz/agreement/presentation/AgreementReadAdapter.java
+++ founderz-presentation/agreement-presentation/src/main/java/com/founderz/agreement/presentation/AgreementReadAdapter.java
@@ -1,4 +1,23 @@
 package com.founderz.agreement.presentation;
 
-public class AgreementReadAdapter {
+import com.founderz.agreement.application.AgreementReadService;
+import com.founderz.agreement.presentation.document.AgreementReadDocumentation;
+import com.founderz.agreement.presentation.response.AgreementDetails;
+import com.founderz.common.presentation.annotation.WebRestAdapter;
+import com.founderz.common.vo.investment.SecuredInvestmentId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@WebRestAdapter("/api/agreement")
+class AgreementReadAdapter implements AgreementReadDocumentation {
+    private final AgreementReadService agreementReadService;
+
+    @GetMapping("/{investmentId}")
+    public AgreementDetails getByInvestmentId(@PathVariable final SecuredInvestmentId investmentId) {
+        final var agreementWithRepayments = agreementReadService.getByInvestmentId(investmentId.toInvestmentId());
+
+        return AgreementDetails.create(agreementWithRepayments.agreementDto(), agreementWithRepayments.repaymentDtos());
+    }
 }
