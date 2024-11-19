c58460206762e9a92bd60b1dda99047e8698d285
dkflfkd53 <b57062562@gmail.com>
2024-11-19 11:09:43 +0900

    add: 마이페이지 조회 api presentation 계층 추가

--- founderz-presentation/user-presentation/src/main/java/com/founderz/user/presentation/UserReadAdapter.java
+++ founderz-presentation/user-presentation/src/main/java/com/founderz/user/presentation/UserReadAdapter.java
@@ -2,12 +2,20 @@ package com.founderz.user.presentation;
 
 import com.founderz.common.presentation.annotation.WebRestAdapter;
 import com.founderz.user.application.UserReadService;
+import com.founderz.user.presentation.document.UserReadDocumentation;
+import com.founderz.user.presentation.response.UserInfoResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @WebRestAdapter("/api/users")
-class UserReadAdapter {
+class UserReadAdapter implements UserReadDocumentation {
     private final UserReadService readService;
 
-    public
+    @GetMapping("/my-info")
+    public UserInfoResponse getUserInfo() {
+        final var userWithInterestsDto = readService.getCurrentUserInfo();
+
+        return UserInfoResponse.create(userWithInterestsDto.userInfo(), userWithInterestsDto.interests());
+    }
 }
